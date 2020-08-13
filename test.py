3c9c4d49662fc264bc8e2585470caca474257503
MaouLuo <mrsama@163.com>
2020-08-13 17:24:03 +0800

    校shaixu 文名‘

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
-import time
 from multiprocessing import Process, Queue
 from random import randint
-from progress.bar import Bar
+import requests
+import threading
+import re
+import time
+import datetime
 
 def foo():
     
@@ -16,12 +19,12 @@ def foo():
 def calculation():
     total = 0
     number_list = [x for x in range(1, 100000001)]
-    start = time()
+    #start = time()
     for number in number_list:
         total += number
     print(total)
-    end = time()
-    print('Execution time: %.3fs' % (end - start))
+    #end = time()
+    #print('Execution time: %.3fs' % (end - start))
 
 def task_handler(curr_list, result_queue):
     total = 0
@@ -42,7 +45,7 @@ def calculation_process():
 		processes.append(p)
 		p.start()
 	# 开始记录所有进程执行完成花费的时间
-	start = time()
+	#start = time()
 	for p in processes:
 		p.join()
 	# 合并执行结果
@@ -50,8 +53,8 @@ def calculation_process():
 	while not result_queue.empty():
 		total += result_queue.get()
 	print(total)
-	end = time()
-	print('Process Execution time: ', (end - start), 's', sep='')
+	#end = time()
+	#print('Process Execution time: ', (end - start), 's', sep='')
 
 def test():
 	f = [x + y for x in 'ABCDE' for y in '1234567']
@@ -66,21 +69,54 @@ def test():
 		print('key:{0}, v:{1}'.format(k,v)) 
 	#print(re.key())
 
+'''
 def test1():
 	bar = Bar('Processing', max=100, fill='@', suffix='%(percent)d%%')
 	for i in range(100):
 		time.sleep(0.1)
 		bar.next()
 	bar.finish()
-
+'''
 
 def test2():
 	for _ in range(10):
-		print(random() + 1)
+	#	print(random() + 1)
+		pass
 
+def sever_test():
+	url = 'https://sc.ftqq.com/SCU77831Td9cccdfd533462659d8d881e9408da945e1ffad3eb8a8.send'
+	data = {
+		'text': 'py'
+	}
+	resp = requests.post(url, data=data)
+	print(resp.text)
+
+def get_date():
+	curr_time = datetime.datetime.now()
+	print(curr_time.strftime("%Y%m%d"))
+
+def get_sina_data():
+	url = 'http://hq.sinajs.cn/list=sh600389'
+	resp = requests.get(url, timeout=20)
+	print(resp.text[0])
+
+def bool2str():
+	a = False
+	b=''
+	print('{} hahah'.format(a))
+	if None == a:
+		print('yes')
+	else:
+		print('no')
+	#print(b is False)
 
 def main():
-	test2()
+	#get_date()
+	#list_e = []
+	#print(list_e is False)
+	#get_sina_data()
+	bool2str()
+
 
 if __name__ == '__main__':
     main()
---
+++ 中文公司名筛选/company_filter.py
@@ -0,0 +1,66 @@
+import pandas as pd
+
+path = 'E:\\可视化\\输出文档\\中文名\\N2S结算单位汇总.xlsx'
+sheet_name = ['开通N2S结算单位', '订单相关结算单位', '账单相关结算单位']
+name_list = [
+    '贺琳',     
+'王雪纯',    
+'向阳',      
+'杨小兰' ,   
+'钟思敏'   , 
+'邹方玲' ,   
+'黎月明' ,   
+'卢翠香' ,   
+'舒乐'  ,    
+'祝梦婷' ,   
+'李镇粤' ,   
+'欧阳婷'  ,  
+'张波'   ,   
+'张小东' ,   
+'蔡浩权' ,   
+'贺根强'   ,  
+'梁健森' ,   
+'刘城林' ,   
+'曾浩炫',    
+'梁诗婷',    
+'刘泽民',    
+'彭英剑' ,   
+'章振坤' ,   
+'郑瀚'  ,    
+'helin', 
+'wangxuechun', 
+'xiangyang', 
+'yangxiaolan', 
+'zhongsimin', 
+'zoufangling', 
+'liyueming', 
+'lucuixiang', 
+'shule', 
+'zhumengting', 
+'lizhenyue',    
+'ouyangting',    
+'zhangbo',      
+'zhangxiaodong' ,   
+'caihaoquan',    
+'hegenqiang',     
+'liangjiansen',    
+'liuchenglin' ,   
+'zenghaoxuan',    
+'liangshiting',    
+'liuzemin',    
+'pengyingjian',    
+'zhangzhenkun',    
+'zhenghan',  
+]
+
+
+def main():
+    #print(name_list[12])
+
+    data = pd.read_excel(path, sheet_name=0, usecols=0)
+    print(data[1])
+
+
+
+
+main()
